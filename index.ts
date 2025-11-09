7897119b50c7d2d67fefd147fa179a1c2bb774cb
dekitate-com <wonderfulkiyo@gmail.com>
2025-11-09 16:58:19 +0900

    chore: biome のバージョンをアップ

--- index.ts
+++ index.ts
@@ -3,5 +3,8 @@ console.log("Hello via Bun!");
 
 function main() {
 	// biome-ignore lint/suspicious/noConsole: This is a Test
+	// biome-ignore lint/complexity/noArguments: This is a Test
 	console.log(arguments);
 }
+
+main();
